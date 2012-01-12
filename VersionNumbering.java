b6d69c9aac30117396a1c782cefb352306f21b58
Raphfrk <raphfrk@gmail.com>
2012-01-12 02:44:29 +0000

    Added license headers
    Signed-off-by: Raphfrk <raphfrk@gmail.com>

--- VersionNumbering.java
+++ VersionNumbering.java
@@ -19,55 +19,50 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  ******************************************************************************/
-package com.raphfrk.bukkit.serverportwarps.command;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
-import org.bukkit.Location;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
-import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
+public class VersionNumbering {
+	final static String version = "7";
 
-import com.raphfrk.bukkit.serverportwarps.ServerPortWarps;
+	static String name = "default";
+	static final String slash = System.getProperty("file.separator");
+	static String prefix = "default";
+	
+	static void updateUpdatr() {
+		
+		if( !(new File( "Updatr")).exists() ) {
+			(new File( "Updatr")).mkdir();
+		}
 
-public class SetWarpCommand implements CommandExecutor {
-
-	ServerPortWarps p;
-
-	public SetWarpCommand(ServerPortWarps p) {
-		this.p = p;
+		try {
+			
+			BufferedWriter bw = new BufferedWriter(new FileWriter("Updatr" + slash + name + ".updatr"));
+			
+			bw.write( "name=" + name );
+			bw.newLine();
+			
+			bw.write( "version=" + version );
+			bw.newLine();
+			
+			bw.write( "url=www.prydwen.net/minec/latest" + prefix + "/" + name + ".updatr");
+			bw.newLine();
+			
+			bw.write( "file=www.prydwen.net/minec/latest" + prefix + "/" + name + ".updatr");
+			bw.newLine();
+			
+			bw.write( "notes=");
+			bw.newLine();
+			
+			bw.close();
+			
+		} catch (IOException ioe) {
+			ioe.printStackTrace();
+		}
+		
+		
+		
 	}
-
-	@Override
-	public boolean onCommand(CommandSender sender, Command command, String label,
-			String[] args) {
-
-		if(args.length < 1) {
-			return false;
-		}
-
-		if(!(sender instanceof Player)) {
-			sender.sendMessage("Only players can set warps");
-			return true;
-		}
-		
-		Player player = (Player)sender;
-		
-		String prefix = "p." + player.getName();
-		
-		String warpName = args[0];
-		
-		Location currentLoc = player.getLocation();
-		
-		boolean success = p.warpManager.setLocation(warpName, currentLoc, prefix, 10);
-		
-		if(success) {
-			sender.sendMessage("Warp " + args[0] + " set to this location");
-			return true;
-		} else {
-			sender.sendMessage("Unable to set warp");
-			return true;
-		}
-	}
-
 }
-
--- src/com/raphfrk/bukkit/serverportwarps/ServerPortWarps.java
+++ src/com/raphfrk/bukkit/serverportwarps/ServerPortWarps.java
@@ -1,11 +1,30 @@
+/*******************************************************************************
+ * Copyright (C) 2012 Raphfrk
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ ******************************************************************************/
 package com.raphfrk.bukkit.serverportwarps;
 
 import java.io.File;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
-
-import javax.persistence.PersistenceException;
 
 import org.bukkit.Server;
 import org.bukkit.event.Event.Priority;
@@ -16,7 +35,6 @@ import org.bukkit.plugin.ServicesManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import com.avaje.ebean.EbeanServer;
-import com.avaje.ebean.cache.ServerCacheManager;
 import com.raphfrk.bukkit.eventlinkapi.EventLinkAPI;
 import com.raphfrk.bukkit.serverportcoreapi.ServerPortCoreAPI;
 import com.raphfrk.bukkit.serverportwarps.command.DeleteWarpCommand;
@@ -90,6 +108,8 @@ public class ServerPortWarps extends JavaPlugin {
 		pm.registerEvent(Type.CUSTOM_EVENT, customListener, Priority.Normal, this);
 
 		warpManager.registerWarps();
+		
+		log("Initialised");
 
 	}
 
